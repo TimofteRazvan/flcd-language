b18ce4e3cad766bafd6c493b686b9a35d408a8c9
937_Timofte_Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2023-12-16 21:09:02 +0200

    Add files via upload

--- Parser/Parser.java
+++ Parser/Parser.java
@@ -7,7 +7,7 @@ public class Parser {
 
     public Parser() {
         this.grammar = new Grammar();
-        grammar.readGrammarFromFile("D:\\University\\Year - 3\\Tehnici de compilare\\Lab3\\Lab3\\src\\g3.txt");
+        grammar.readGrammarFromFile("D:\\University\\Year - 3\\Tehnici de compilare\\Lab3\\Lab3\\src\\g4.txt");
         this.firstSets = calculateFirstSets(grammar);
         this.followSets = calculateFollowSets(grammar, firstSets);
     }
@@ -15,129 +15,113 @@ public class Parser {
     private Map<String, Set<String>> calculateFirstSets(Grammar grammar) {
         Map<String, Set<String>> firstSets = new HashMap<>();
         Set<String> nonTerminals = grammar.getNonTerminals();
-
+    
         for (String nonTerminal : nonTerminals) {
             firstSets.put(nonTerminal, new HashSet<>());
         }
-
+    
         boolean changed;
         do {
             changed = false;
+    
             for (String nonTerminal : nonTerminals) {
                 List<String> productions = grammar.getProductionsForNonTerminal(nonTerminal);
-
+    
                 for (String production : productions) {
                     String[] symbols = production.split("\\s+");
                     int i = 0;
+                    boolean hasEpsilon = true;
+    
                     while (i < symbols.length) {
                         String symbol = symbols[i];
-
-                        if (grammar.isTerminal(symbol)) {
-                            changed = changed || firstSets.get(nonTerminal).add(symbol);
+    
+                        if (symbol.equals("e")) {
+                            changed |= firstSets.get(nonTerminal).add(symbol);
+                            hasEpsilon = false;
                             break;
-                        } else {
-                            Set<String> symbolFirstSet = new HashSet<>();
-                            if (firstSets.containsKey(symbol)) {
-                                symbolFirstSet.addAll(firstSets.get(symbol));
-                                symbolFirstSet.remove(""); // Remove epsilon if it exists
-                            } else {
-                                symbolFirstSet.add("");
-                            }
-                            changed = changed || firstSets.get(nonTerminal).addAll(symbolFirstSet);
-
-                            if (!symbolFirstSet.contains("")) {
+                        } else if (grammar.isTerminal(symbol)) {
+                            System.out.println(symbol);
+                            changed |= firstSets.get(nonTerminal).add(symbol);
+                            hasEpsilon = false;
+                            break;
+                        } else if (firstSets.containsKey(symbol)) {
+                            Set<String> symbolFirstSet = new HashSet<>(firstSets.get(symbol));
+                            symbolFirstSet.remove("e");
+    
+                            changed |= firstSets.get(nonTerminal).addAll(symbolFirstSet);
+    
+                            if (!symbolFirstSet.contains("e")) {
+                                hasEpsilon = false;
                                 break;
                             }
+                        } else {
+                            hasEpsilon = false;
+                            break;
                         }
                         i++;
                     }
-
-                    if (i == symbols.length) {
-                        changed = changed || firstSets.get(nonTerminal).add("");
+    
+                    if (hasEpsilon) {
+                        changed |= firstSets.get(nonTerminal).add("e");
                     }
                 }
             }
         } while (changed);
-
+    
         return firstSets;
-    }
-
+    }     
+        
     private Map<String, Set<String>> calculateFollowSets(Grammar grammar, Map<String, Set<String>> firstSets) {
         Map<String, Set<String>> followSets = new HashMap<>();
         Set<String> nonTerminals = grammar.getNonTerminals();
-        followSets.put(grammar.getStartingSymbol(), Set.of("$"));
-
+    
         for (String nonTerminal : nonTerminals) {
             followSets.put(nonTerminal, new HashSet<>());
         }
-
+    
+        followSets.get(grammar.getStartingSymbol()).add("$");
+    
         boolean changed;
         do {
             changed = false;
-
+    
             for (String nonTerminal : nonTerminals) {
-                for (Map.Entry<String, List<String>> entry : grammar.getProductions().entrySet()) {
-                    String leftHandSide = entry.getKey();
-                    List<String> productionList = entry.getValue();
-
-                    for (String production : productionList) {
-                        int idx = production.indexOf(nonTerminal);
-
-                        while (idx >= 0) {
-                            int followIdx = idx + nonTerminal.length();
-                            if (followIdx < production.length()) {
-                                String rest = production.substring(followIdx).trim();
-
-                                if (!rest.isEmpty()) {
-                                    String[] symbols = rest.split("\\s+");
-                                    for (String symbol : symbols) {
-                                        if (grammar.isNonTerminal(symbol)) {
-                                            Set<String> followSet = followSets.get(symbol);
-                                            Set<String> firstOfRest = calculateFirstOfRest(grammar, firstSets, symbol, rest);
-                                            changed |= followSet.addAll(firstOfRest);
-                                            if (!firstOfRest.contains("e")) {
-                                                break;
-                                            }
-                                        } else if (grammar.isTerminal(symbol)) {
-                                            changed |= followSets.get(nonTerminal).add(symbol);
-                                            break;
-                                        }
+                List<String> productions = grammar.getProductionsForNonTerminal(nonTerminal);
+    
+                for (String production : productions) {
+                    String[] symbols = production.split("\\s+");
+                    int len = symbols.length;
+    
+                    for (int i = 0; i < len; i++) {
+                        String currentSymbol = symbols[i];
+    
+                        if (grammar.isNonTerminal(currentSymbol)) {
+                            if (i < len - 1) {
+                                String nextSymbol = symbols[i + 1];
+    
+                                if (grammar.isTerminal(nextSymbol)) {
+                                    changed |= followSets.get(currentSymbol).add(nextSymbol);
+                                } else if (grammar.isNonTerminal(nextSymbol)) {
+                                    Set<String> nextFirstSet = new HashSet<>(firstSets.get(nextSymbol));
+                                    nextFirstSet.remove("e");
+    
+                                    changed |= followSets.get(currentSymbol).addAll(nextFirstSet);
+    
+                                    if (firstSets.get(nextSymbol).contains("e")) {
+                                        changed |= followSets.get(currentSymbol).addAll(followSets.get(nonTerminal));
                                     }
-                                } else {
-                                    changed |= followSets.get(nonTerminal).addAll(followSets.get(leftHandSide));
                                 }
+                            } else {
+                                changed |= followSets.get(currentSymbol).addAll(followSets.get(nonTerminal));
                             }
-
-                            idx = production.indexOf(nonTerminal, idx + 1);
                         }
                     }
                 }
             }
         } while (changed);
-
+    
         return followSets;
-    }
-
-    private Set<String> calculateFirstOfRest(Grammar grammar, Map<String, Set<String>> firstSets, String symbol, String rest) {
-        String[] symbols = rest.split("\\s+");
-        Set<String> firstSet = new HashSet<>();
-
-        for (String s : symbols) {
-            if (grammar.isTerminal(s)) {
-                firstSet.add(s);
-                return firstSet;
-            } else if (grammar.isNonTerminal(s)) {
-                Set<String> first = firstSets.get(s);
-                firstSet.addAll(first);
-                if (!first.contains("e")) {
-                    return firstSet;
-                }
-            }
-        }
-
-        firstSet.addAll(followSets.get(symbol));
-        return firstSet;
-    }
+    }       
 
     public void printFirstAndFollowSets() {
         System.out.println("FIRST sets:");
